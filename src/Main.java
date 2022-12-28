d7cde098cc485a78f6d15a7be4466ae9796476a2
Vladimir <sharubin@sputnikfund.ru>
2022-12-28 13:54:06 +0700

    переписал весь код

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,65 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
-
-
     public static void main(String[] args) {
+        while (true) {
+            System.out.println("Приветствую! Какие игридиенты вы хотите использовать для готовки? Перечислите номераингридиентов через пробел\n1-лук, 2-морковка, 3-рис, 4-мясо, 5-курица, 6-гречка, 7-макароны");
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            String[] s = str.split(" ");
+            int[] ing = new int[s.length];
+            for (int j = 0; j < s.length; j++) {
+                ing[j] = Integer.parseInt(s[j]);
+            }
+            ArrayList<String> dishOutput = new ArrayList<>();
 
-        System.out.println("РџСЂРёРІРµС‚СЃС‚РІСѓСЋ! РљР°РєРёРµ РёРіСЂРёРґРёРµРЅС‚С‹ РІС‹ С…РѕС‚РёС‚Рµ РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ РґР»СЏ РіРѕС‚РѕРІРєРё? РџРµСЂРµС‡РёСЃР»РёС‚Рµ С‡РµСЂРµР· РїСЂРѕР±РµР» С†РёС„СЂС‹ \n1-Р»СѓРє, 2-РјРѕСЂРєРѕРІРєР°, 3-СЂРёСЃ, 4-РјСЏСЃРѕ, 5-РєСѓСЂРёС†Р°, 6-РіСЂРµС‡РєР°, 7-РјР°РєР°СЂРѕРЅС‹");
-        int[] plov = new int[] {1, 2, 3, 4};
-        int[] sup = new int[] {1,2,5,6,7};
-        int plovCount = 0;
-        int supCount = 0;
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] s = str.split(" ");
-        int[] ing = new int[s.length];
-        for (int j = 0; j < s.length; j++) {
-            ing[j]=Integer.parseInt(s[j]);
-        }
-        for (int i = 0; i < ing.length; i++) {
-            for (int i1 = 0;ing.length >= plov.length & i1 < plov.length; i1++) {
-                if (ing[i]==plov[i1]) plovCount++;
+            if (isPlovMatched(ing)) dishOutput.add("Плов");
+            if (isSupMatched(ing)) dishOutput.add("Суп");
+            String separator = ", ";
+            String stringDishOutput = String.join(separator, dishOutput);
+            if (dishOutput.isEmpty()) {
+                System.out.println("К сожалению вы ничего не можете приготовить :(\nХотите попробовать снова?");
+                String answer = sc.nextLine();
+                if (answer.equals("нет")) break;
             }
-            for (int i1 = 0; ing.length >= sup.length & i1 < sup.length; i1++) {
-                if (ing[i]==sup[i1]) supCount++;
+            else {
+                System.out.println("Вы можете приготовить: " + stringDishOutput + "\nХотите выбрать 1 случайное блюдо?");
+                String answer = sc.nextLine();
+                if (answer.equals("да")) {
+                    System.out.println(dishOutput.get(0+(int) (Math.random() * dishOutput.size())));
+                    break;
+                }
+                if (answer.equals("нет")) {
+                    System.out.println("Тогда продублирую список: " + stringDishOutput);
+                    break;
+                }
             }
+
         }
-        if (plovCount==plov.length) System.out.println("РџР»РѕРІ");
-         if (supCount==sup.length) System.out.println("РЎСѓРї");
-         else System.out.println("Рљ СЃРѕР¶Р°Р»РµРЅРёСЋ РёР· РґР°РЅРЅС‹С… РёРЅРіСЂРёРґРёРµРЅС‚РѕРІ РЅРёС‡РµРіРѕ РЅРµ РїСЂРёРіРѕС‚РѕРІРёС‚СЊ((");
     }
 
-
+    public static boolean isPlovMatched(int[] ings) {
+        int plovCount = 0;
+        int[] plov = new int[] {1, 2, 3, 4};
+        for (int i = 0; i < ings.length; i++) {
+            int thisIng = ings[i];
+            for (int j = 0; j < plov.length; j++) {
+                if (thisIng==plov[j]) plovCount++;
+            }
+        }
+        if (plovCount==plov.length) return true;
+        else return false;
+    }
+    public static boolean isSupMatched(int[] ings) {
+        int supCount = 0;
+        int[] sup = new int[] {1,2,5,6,7};
+        for (int i = 0; i < ings.length; i++) {
+            int thisIng = ings[i];
+            for (int j = 0; j < sup.length; j++) {
+                if (thisIng==sup[j]) supCount++;
+            }
+        }
+        if (supCount==sup.length) return true;
+        else return false;
+    }
 }
